f46a1464e588701b749c0b94c17915079426ea39
Gaurav <gaurav@Gauravs-MacBook-Pro.local>
2025-10-29 16:36:13 +0530

    fix error

--- src/app/api/refresh/route.ts
+++ src/app/api/refresh/route.ts
@@ -1,8 +1,8 @@
 import { revalidateTag } from "next/cache";
 
 export const GET = (req: Request) => {
-  revalidateTag("projects-list");
-  revalidateTag("all-projects");
-  revalidateTag("more-projects");
+  revalidateTag("projects-list","max");
+  revalidateTag("all-projects","max");
+  revalidateTag("more-projects","max");
   return new Response("Refreshed", { status: 200 });
 };
